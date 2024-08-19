889cb81ed79630b51923f5352f7b7914720327dd
Natalia <natalia.ribeiro@globalfinanceiro.com.br>
2024-08-19 15:44:54 -0300

    adiciona os cards na home e cria a função de chamada da api

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Card, HomeServiceService } from '../../home-service.service';
+import { ProductsCardComponent } from '../../components/products-card/products-card.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [ProductsCardComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+  cards: Card[] = [];
+  isLoading = true;
+  notFoundMessage = '';
 
+  constructor(private homeService: HomeServiceService) {}
+  ngOnInit(): void {
+    this.loadCards();
+  }
+
+  loadCards(): void {
+    this.homeService.getCards().subscribe({
+      next: (data) => {
+        this.cards = data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Error searching cards', err);
+        this.isLoading = false;
+      }
+    })
+  }
 }
